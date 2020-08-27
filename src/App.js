994a3b8014f2faa4a683c53e20f2fef246740ebd
Szczepan <s.szablej1@gmail.com>
2020-08-27 17:58:06 +0200

    :sparkles: Add search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
 import "./App.css";
+import fetchData from "./api/fetchData";
+import { useState } from "react";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchData(inputValue);
+      setWeather(data);
+      setInputValue("");
+      console.log(data);
+    }
+  };
+
   return (
     <div>
-      <h1>Hello Weather App!</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temp. {weather.main.temp}</p>
+          <p>Real feel {weather.main.feels_like}</p>
+          <p>Min. temp {weather.main.temp_min}</p>
+          <p>Max. temp {weather.main.temp_max}</p>
+        </div>
+      )}
     </div>
   );
 };
